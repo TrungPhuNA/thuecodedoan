535541a8267ec4c2a42e6d636ccf9631efeb2f28
Phan Trung Phú <phupt@dol-tech.com>
2021-03-27 03:50:09 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('get.project');
 });
 
 Route::prefix('doan')->group(function (){
